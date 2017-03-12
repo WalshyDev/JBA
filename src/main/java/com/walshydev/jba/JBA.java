aacddfdb39e67e0ec853df0de3f116579865b54b
bwfcwalshyPluginDev <bwfcwalshy@gmail.com>
2017-03-12 20:12:42 +0000

    * Implement sharding into the init process.

--- src/main/java/com/walshydev/jba/JBA.java
+++ src/main/java/com/walshydev/jba/JBA.java
@@ -21,7 +21,8 @@ public abstract class JBA {
 
     private static JBA instance;
 
-    private JDA client;
+    private JDA[] clients;
+    private int shards;
     private String prefix;
     private static String version;
 
@@ -41,22 +42,104 @@ public abstract class JBA {
     }
 
     /**
+     * Use this method to create the bot, this will make a client for you.
+     *
+     * If you want to use custom prefix's for example override the {@link JBA#getPrefix(Guild)} method and make that return whatever is needed.
+     * @param type The {@link AccountType} of bot.
+     * @param token The bot token.
+     */
+    public JDA init(AccountType type, String token){
+        return init(new JDABuilder(type).setToken(token), null);
+    }
+
+    /**
      * Use this method to create the bot, pass the JDA client and bot prefix to setup the command system to use a given prefix.
      *
      * If you want to use custom prefix's for example override the {@link #getPrefix(Guild)} method and make that return whatever is needed.
-     * @param jdaBuilder
-     * @param botPrefix
+     * @param jdaBuilder JDABuilder, this is good if you want to set an audio factory for example.
+     * @param botPrefix The prefix of your bot.
      */
     public JDA init(JDABuilder jdaBuilder, String botPrefix){
+        return init(jdaBuilder, 1, botPrefix);
+    }
+
+    /**
+     * Use this method to create the bot, this will make a client for you and passing the bot prefix will setup the command system to use a given prefix.<br>
+     * This method will also shard with the given count.
+     *
+     * If you want to use custom prefix's for example override the {@link JBA#getPrefix(Guild)} method and make that return whatever is needed.
+     * @param type The {@link AccountType} of bot.
+     * @param token The bot token.
+     * @param botPrefix The prefix of your bot.
+     */
+    public JDA init(AccountType type, String token, String botPrefix, int shards){
+        return init(new JDABuilder(type).setToken(token), shards, botPrefix);
+    }
+
+    /**
+     * Use this method to create the bot, this will make a client for you.<br>
+     * This method will also shard with the given count.
+     *
+     * If you want to use custom prefix's for example override the {@link JBA#getPrefix(Guild)} method and make that return whatever is needed.
+     * @param type The {@link AccountType} of bot.
+     * @param token The bot token.
+     */
+    public JDA init(AccountType type, String token, int shards){
+        return init(new JDABuilder(type).setToken(token), shards);
+    }
+
+    /**
+     * Use this method to create the bot, pass the JDA client and bot prefix to setup the command system to use a given prefix.
+     *
+     * If you want to use custom prefix's for example override the {@link #getPrefix(Guild)} method and make that return whatever is needed.
+     * @param jdaBuilder JDABuilder, this is good if you want to set an audio factory for example.
+     */
+    public JDA init(JDABuilder jdaBuilder, int shards){
+        return init(jdaBuilder, shards, null);
+    }
+
+    /**
+     * Use this method to create the bot, pass the JDA client and bot prefix to setup the command system to use a given prefix.
+     *
+     * If you want to use custom prefix's for example override the {@link #getPrefix(Guild)} method and make that return whatever is needed.
+     * @param jdaBuilder JDABuilder, this is good if you want to set an audio factory for example.
+     * @param shards The amount of shards you with to use.
+     * @param botPrefix The prefix of your bot.
+     */
+    public JDA init(JDABuilder jdaBuilder, int shards, String botPrefix) {
         instance = this;
         jdaBuilder.addListener(new JBAListener());
-        try {
-            this.client = jdaBuilder.buildBlocking();
-        } catch (LoginException | InterruptedException | RateLimitedException e) {
-            LOGGER.error("Failed to build client", e);
+        clients = new JDA[shards];
+        this.shards = shards;
+        if(clients.length == 1) {
+            try {
+                this.clients[0] = jdaBuilder.buildBlocking();
+            } catch ( RateLimitedException e) {
+                LOGGER.error("Failed to build client! Retrying in " + e.getRetryAfter() + " milliseconds", e);
+                try {
+                    Thread.sleep(e.getRetryAfter());
+                } catch (InterruptedException e1) {}
+            } catch (LoginException | InterruptedException e) {
+                LOGGER.error("Failed to login!", e);
+            }
+        }else{
+            for(int i = 0; i < clients.length; i++){
+                while(true) {
+                    try{
+                        clients[i] = jdaBuilder.useSharding(i, clients.length).buildAsync();
+                    } catch (RateLimitedException e) {
+                        LOGGER.error("Failed to build client! Retrying in " + e.getRetryAfter() + " milliseconds", e);
+                        try {
+                            Thread.sleep(e.getRetryAfter());
+                        } catch (InterruptedException e1) {}
+                    } catch (LoginException e) {
+                        LOGGER.error("Failed to login!", e);
+                    }
+                }
+            }
         }
         this.prefix = botPrefix;
-        return client;
+        return clients[0];
     }
 
     /**
@@ -77,12 +160,21 @@ public abstract class JBA {
     }
 
     /**
-     * This is called when the ReadyEvent is fired, call the {@link #registerCommand(Command)} method in this method.
+     * This is called when the ReadyEvent is fired, call the {@link #registerCommand(Command)} method in this method.<br>
+     * If sharding is used this will always return the first shard.
      */
     public abstract void run();
 
     public JDA getClient(){
-        return this.client;
+        return this.clients[0];
+    }
+
+    public JDA[] getClients(){
+        return this.clients;
+    }
+
+    public int getTotalShards(){
+        return this.shards;
     }
 
     /**
